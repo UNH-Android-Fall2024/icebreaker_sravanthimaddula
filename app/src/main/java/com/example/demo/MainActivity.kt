21008c2a3f7fc6416d295cf22fad46f8e4bf8aa3
sravanthi maddula <smadd25@unh.newhaven.edu>
2024-09-29 10:07:43 -0500

    final demo app

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,53 +1,49 @@
 package com.example.demo
 
-
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.demo.databinding.ActivityMainBinding
+import com.example.demo.questions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.example.demo.databinding.ActivityMainBinding
 
-// Main activity where my code starts
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val className = "android-fall24"
+    private val className = "F24"
     private val db = Firebase.firestore
     private var TAG = "IcebreakerF24-TAG"
     private var questionBank: MutableList<questions>? = arrayListOf()
 
-    // Override the onCreate Fucntion and do stuff
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         getQuestionsFromFirebase()
 
         binding.btnSetRandomQuestion.setOnClickListener {
-            binding.txtQuestion.text = questionBank!!.random().text
+            val randomQuestion = questionBank!!.random().text
+            binding.txtQuestion.text = randomQuestion
         }
 
         binding.btnSubmit.setOnClickListener {
             writeStudentToFirebase()
-            binding.txtquestion.text = ""
-
         }
     }
 
-    // Function to get question from database
     private fun getQuestionsFromFirebase(){
-        db.collection("Questions")
+        db.collection("questions")
             .get()
             .addOnSuccessListener { result ->
                 questionBank = mutableListOf()
-                for(document in result) {
-                    val question = document.toObject(Questions::class.java)
-                    questionBank!!.add(question) // Necessary
+                for(document in result){
+                    val question = document.toObject(questions::class.java)
+                    questionBank!!.add(question)
                     Log.d(TAG,"$question")
                 }
             }
@@ -63,29 +59,39 @@ class MainActivity : AppCompatActivity() {
         val answer = binding.txtAnswer
 
         Log.d(TAG, "Variables: $firstName $lastName $prefName $answer")
+        Log.d(TAG, "Question being written: ${binding.txtQuestion.text.toString()}")
 
-        // Convert data to hasmap for firebase purposes
         val student = hashMapOf(
-            "firstname"  to firstName.text.toString(),
-            "lastname"  to lastName.text.toString(),
-            "prefname"  to prefName.text.toString(),
-            "answer"    to answer.text.toString(),
-            "class"     to className,
-            "question"  to binding.txtQuestion.text.toString()
+            "firstname" to firstName.text.toString(),
+            "lastname" to lastName.text.toString(),
+            "prefname" to prefName.text.toString(),
+            "answer" to answer.text.toString(),
+            "class" to className,
+            "question" to binding.txtQuestion.text.toString()
         )
-        // Access Firebase database
+
         db.collection("students")
             .add(student)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot written successfully with ID: ${documentReference.id}")
+            .addOnSuccessListener{documentReference ->
+                Log.d(TAG,"Document Snapshot written successfully with ID: ${documentReference.id}")
             }
             .addOnFailureListener { exception ->
-                Log.w(TAG, "Error adding document", exception)
+                Log.w(TAG, "Error adding Document", exception)
             }
-
         firstName.setText("")
         lastName.setText("")
         prefName.setText("")
         answer.setText("")
+        binding.txtQuestion.text = ""
+
     }
+
+
+    /*
+    setContentView(R.layout.activity_main)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Header1)) { v, insets ->
+        val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        insets
+    }*/
 }
